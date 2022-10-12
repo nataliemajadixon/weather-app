4730bb8f8c77610d916766bba40a194ee64a1e13
natolay <ndixon@seattleu.edu>
2022-10-12 16:40:19 +0200

    created new header and added current temp

--- script.js
+++ script.js
@@ -1,32 +1,48 @@
-var button1 = document.querySelector('.button1')
+var button1 = document.querySelector('.button1');
 
-var inputValue = document.querySelector('.input-value')
+var information1 = document.querySelector("#information1") ;
 
-var nameC = document.querySelector('.name');
-var desc = document.querySelector('.desc');
-var temp = document.querySelector('.temp');
+var inputValue = document.querySelector('.input-value');
 
-button1.addEventListener( 'click', function(){
+// var nameC = document.querySelector('.name');
+// var desc = document.querySelector('.desc');
+// var temp = document.querySelector('.temp');
 
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' +inputValue.value+ '&appid=f94db0bc7c75590e5b53bfe2a0feeab9')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        var nameValue = data['name'];
-        var temp = data['main']['temp'];
-        var descValue = data['weather']['0']['description'];
-        
-        nameC.innerHTML = nameValue;
-        temp.innerHTML = tempValue;
-        desc.innerHTML = descValue;
+    function getWeather(){
+        const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=' +inputValue.value+ '&units=imperial&appid=f94db0bc7c75590e5b53bfe2a0feeab9'
 
-    })
+        fetch(apiURL)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            let cityName = document.createElement('p');
+            cityName.innerHTML = data.name
+            information1.innerHTML = `The tempurature is   ${data.main.temp}` 
+            information1.append(cityName)
+console.log(information1);
 
+
+            // var nameValue = data['name'];
+            // var temp = data['main']['temp'];
+            // var descValue = data['weather']['0']['description'];
+            
+            // nameC.innerHTML = nameValue;
+            // temp.innerHTML = tempValue;
+            // desc.innerHTML = descValue;
     
+        })
+    
+        
+    
+        .catch(err => alert("Wrong city name!"))
+    }
 
-    .catch(err => alert("Wrong city name!"))
+button1.addEventListener( 'click', getWeather)
 
-})
+
+   
+
+
 
 
 
